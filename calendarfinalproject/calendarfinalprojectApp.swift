ac458a1a3eb7aa7f07483ab5a49096c4e631b40a
Isabelle Tuyay <iylabelle03@gmail.com>
2024-06-26 14:45:10 -0500

    Initial Commit

---
+++ calendarfinalproject/calendarfinalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarfinalprojectApp.swift
+//  calendarfinalproject
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct calendarfinalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
